ec3b476489c08a4ffe664b43da6676fa68a6bd3c
finley <sssrrr@vip.qq.com>
2018-05-16 21:01:35 +0800

    :sparkles: rap+mock+最新热门商品渲染

---
+++ src/modules/js/api.js
@@ -0,0 +1,14 @@
+let url = {
+  hotLists: '/index/hotList'
+}
+//开发环境和真是环境切换
+// let host=""
+let host = 'http://rapapi.org/mockjsdata/34279'
+
+for (let key in url) {
+  if (url.hasOwnProperty(key)) {
+    url[key] = host + url[key]
+  }
+}
+
+export default url
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,23 @@
 import '@/modules/css/common.css'
 import './index.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import url from '@/modules/js/api.js'
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    lists: null
+  },
+  created() {
+    axios.post(url.hotLists, {
+        pageNum: 1,
+        pageSize: 6
+      })
+      .then(data => {
+        console.log(data);
+        this.lists=data.data.lists
+      })
+  }
+})
