d8a3f8d0e2a5dd4f3593e5b8e7aa45f3aa5e5e6b
finley <sssrrr@vip.qq.com>
2018-05-17 11:32:58 +0800

    :art: foot组件化

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,14 @@
-import '@/modules/css/common.css'
+import 'css/common.css'
 import './index.css'
 
 import Vue from 'vue'
 import axios from 'axios'
-import url from '@/modules/js/api.js'
+import url from 'js/api.js'
 
-import {
-  InfiniteScroll
-} from 'mint-ui'
+import Foot from 'components/Foot.vue'
+
+//mint-ui
+import { InfiniteScroll } from 'mint-ui'
 Vue.use(InfiniteScroll)
 
 let app = new Vue({
@@ -16,7 +17,7 @@ let app = new Vue({
     lists: null,
     pageNum: 1,
     loading: false,
-    loadingOver:false,
+    loadingOver: false
   },
   created() {
     this.getList()
@@ -26,23 +27,27 @@ let app = new Vue({
       if (this.loadingOver) return
       this.loading = true
       axios.post(url.hotLists, {
-        pageNum: this.pageNum,
-        pageSize: 10
-      }).then(data => {
-        let currentList = data.data.lists
-        //数据加载完毕
-        if (currentList.lenght<10) {
-          this.loadingOver=true
-        }
-        //渲染数据
-        if (this.lists) {
-          this.lists = this.lists.concat(currentList)
-        } else {
-          this.lists = currentList
-        }
-        this.loading = false
-        this.pageNum+=1
-      })
+          pageNum: this.pageNum,
+          pageSize: 10
+        })
+        .then(data => {
+          let currentList = data.data.lists
+          //全部数据加载完毕
+          if (currentList.lenght < 10) {
+            this.loadingOver = true
+          }
+          //渲染数据
+          if (this.lists) {
+            this.lists = this.lists.concat(currentList)
+          } else {
+            this.lists = currentList
+          }
+          this.loading = false
+          this.pageNum += 1
+        })
     }
+  },
+  components: {
+    Foot
   }
 })
